aab63babc59e16516ff4a55071709d9de132a5b4
Ashish Reddy <bashish.iitg@gmail.com>
2020-05-16 23:37:41 -0400

    Project 3

---
+++ Prj3_rearr_elements_problem3.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 11:44:18 2020
+
+@author: ashishbasireddy
+"""
+
+def mergesort(items):
+
+    if len(items) <= 1:
+        return items
+    
+    mid = len(items) // 2
+    left = items[:mid]
+    right = items[mid:]
+    
+    left = mergesort(left)
+    right = mergesort(right)
+    
+    return merge(left, right)
+    
+def merge(left, right):
+    
+    merged = []
+    left_index = 0
+    right_index = 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+        
+    return merged
+
+
+def rearrange_digits(input_list):
+    """
+    Rearrange Array Elements so as to form two number such that their sum is maximum.
+
+    Args:
+       input_list(list): Input List
+    Returns:
+       (int),(int): Two maximum sums
+    """
+    sorted_list = list()
+    sorted_list = mergesort(input_list)
+    #print(sorted_list)
+    int1 = 0
+    int2 = 0
+    largest_index = 0
+    
+    if len(sorted_list) % 2 != 0:
+        largest_index = sorted_list.pop(-1)
+
+    #print(sorted_list)
+    for i in range(len(sorted_list)//2):
+        int1 += sorted_list[2*i]* (10**i)
+        int2 += sorted_list[2*i+1]* (10**i)
+    
+    if largest_index:
+        int2 += largest_index * (10**(len(sorted_list)//2))
+    
+    #print(int1)
+    #print(int2)
+    return [int2, int1]
+    
+input_list = [1, 2, 3, 4, 5]
+rearrange_digits(input_list)
+
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
+
+test_function([[1, 2, 3, 4, 5], [542, 31]])
+test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function(test_case)
+
---
+++ prj3_rot_arr_search_problem2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 10:14:57 2020
+
+@author: ashishbasireddy
+"""
+
+def rotated_array_search(input_list, number):
+    """
+    Find the index by searching in a rotated sorted array
+
+    Args:
+       input_list(array), number(int): Input array to search and the target
+    Returns:
+       int: Index or -1
+    """
+    start_index = 0
+    end_index = len(input_list) - 1
+    
+    while start_index <= end_index :
+        
+        middle_index = (start_index + end_index)//2
+        print(middle_index)
+        #print(input_list[middle_index])
+        
+        if input_list[middle_index] == number:
+            return middle_index
+        
+        # Check to see if the sub-array is sorted array
+        if input_list[middle_index] > input_list[start_index]:
+            
+            # Check whether number is in the sorted array
+            if number < input_list[middle_index] and number >= input_list[start_index] :
+                end_index = middle_index - 1
+            else:
+                 start_index = middle_index + 1
+        else: # smaller rotated array
+            if number > input_list[middle_index] and number <= input_list[end_index]:
+                start_index = middle_index + 1
+            else:
+                end_index = middle_index - 1
+        
+    return -1
+
+
+def linear_search(input_list, number):
+    for index, element in enumerate(input_list):
+        if element == number:
+            return index
+    return -1
+
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+        print("Pass")
+    else:
+        print("Fail")
+
+#test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+#test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 10])
